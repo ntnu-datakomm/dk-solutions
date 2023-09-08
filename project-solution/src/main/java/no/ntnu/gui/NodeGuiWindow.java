2f172a5a74dd0f441a9600c3225459d1c8c3fe75
Girts Strazdins <girts.strazdins@gmail.com>
2023-09-08 11:43:34 +0200

    Show static sensors and actuators in node GUI

--- project-solution/src/main/java/no/ntnu/gui/NodeGuiWindow.java
+++ project-solution/src/main/java/no/ntnu/gui/NodeGuiWindow.java
@@ -5,10 +5,13 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import no.ntnu.greenhouse.Actuator;
+import no.ntnu.greenhouse.ActuatorCollection;
 import no.ntnu.greenhouse.ActuatorListener;
 import no.ntnu.greenhouse.Sensor;
 import no.ntnu.greenhouse.SensorActuatorNode;
@@ -18,6 +21,11 @@ import no.ntnu.greenhouse.SensorListener;
  * Window with GUI for overview and control of one specific sensor/actuator node.
  */
 public class NodeGuiWindow extends Stage implements SensorListener, ActuatorListener {
+  private static final int VERTICAL_OFFSET = 50;
+  private static final int HORIZONTAL_OFFSET = 100;
+  private static final double WINDOW_WIDTH = 300;
+  private static final double WINDOW_HEIGHT = 300;
+  private static final double HUGE_HEIGHT = 5000;
   private final SensorActuatorNode node;
 
   /**
@@ -27,9 +35,22 @@ public class NodeGuiWindow extends Stage implements SensorListener, ActuatorList
    */
   public NodeGuiWindow(SensorActuatorNode node) {
     this.node = node;
-    Scene scene = new Scene(createContent(), 200, 100);
+    Scene scene = new Scene(createContent(), WINDOW_WIDTH, WINDOW_HEIGHT);
     setScene(scene);
     setTitle("Node " + node.getId());
+    initializeListeners(node);
+    setPositionAndSize();
+  }
+
+  private void setPositionAndSize() {
+    setX(node.getId() * HORIZONTAL_OFFSET - (double) HORIZONTAL_OFFSET / 2.0);
+    setY(node.getId() * VERTICAL_OFFSET);
+    setMinWidth(WINDOW_HEIGHT);
+    setMinHeight(WINDOW_WIDTH);
+  }
+
+
+  private void initializeListeners(SensorActuatorNode node) {
     setOnCloseRequest(windowEvent -> shutDownNode());
     node.addSensorListener(this);
     node.addActuatorListener(this);
@@ -44,11 +65,39 @@ public class NodeGuiWindow extends Stage implements SensorListener, ActuatorList
   }
 
   private Node createSensorSection() {
-    return null; // TODO
+    VBox vbox = new VBox();
+    node.getSensors().forEach(sensor ->
+        vbox.getChildren().add(
+            new Label(sensor.getType() + ": " + sensor.getCurrent() + sensor.getUnit())
+        )
+    );
+    stretchVertically(vbox);
+    return new TitledPane("Sensors", vbox);
   }
 
   private Node createActuatorSection() {
-    return null;
+    VBox vbox = new VBox();
+    TitledPane actuatorPane = new TitledPane("Actuators", vbox);
+    addActuatorControls(vbox);
+    stretchVertically(actuatorPane);
+    return actuatorPane;
+  }
+
+  private void addActuatorControls(Pane parent) {
+    for (Actuator actuator : node.getActuators()) {
+      String onOff = actuator.isOn() ? "ON" : "off";
+      parent.getChildren().add(new Label(actuator.getType() + ": " + onOff)); // !!!
+    }
+  }
+
+  /**
+   * Ensure that this node always try to get as much vertical space as is available to it
+   * (proportionally to other siblings).
+   *
+   * @param region The region to stretch vertically
+   */
+  private void stretchVertically(Region region) {
+    region.setPrefHeight(HUGE_HEIGHT);
   }
 
   @Override
